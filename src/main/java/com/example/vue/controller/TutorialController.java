75e5599f0de99fb7fe61157c29ea0924c04247dd
keer2345 <keer2345@gmail.com>
2021-04-23 22:08:28 +0800

    CrossOrigin

--- src/main/java/com/example/vue/controller/TutorialController.java
+++ src/main/java/com/example/vue/controller/TutorialController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping(path = "/api")
 public class TutorialController {
